c339bb6917270382f0c69e74fcc2f1b491375ea0
elasticspoon <mail@yuribocharov.dev>
2025-04-26 20:04:43 -0400

    feat: child processes

---
+++ child_processes.rb
@@ -0,0 +1,11 @@
+def run_child
+  puts "Child process started with PID: #{Process.pid}"
+  puts "Child parent PID: #{Process.ppid}"
+end
+
+def start_parent(number_of_children)
+  puts "Parent process started with PID: #{Process.pid}"
+  number_of_children.times { Process.fork { run_child } }
+end
+
+start_parent 3
