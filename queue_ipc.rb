bd94b89e0c07228a32823c7a9fb07e2f72a12284
elasticspoon <mail@yuribocharov.dev>
2025-04-28 03:33:38 -0400

    feat: queue ipc

---
+++ queue_ipc.rb
@@ -0,0 +1,18 @@
+queue = Thread::Queue.new
+
+def read_from_queue(queue)
+  until queue.empty?
+    puts 'Reading from queue...'
+    item = queue.pop
+    puts "Got #{item}"
+    sleep 2
+  end
+ensure
+  puts 'Reader finished'
+  queue.close
+end
+
+10.times { |i| queue << i }
+threads = (1..4).map { Thread.new { read_from_queue(queue) } }
+
+threads.each(&:join)
