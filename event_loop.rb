824818fe4c14e9e0548d9c4dbfb653f98341598a
elasticspoon <mail@yuribocharov.dev>
2025-05-15 00:04:49 -0400

    feat: event loop

---
+++ event_loop.rb
@@ -0,0 +1,25 @@
+queue = Queue.new
+
+class Event
+  def initialize(name, &callback)
+    @name = name
+    @callback = callback
+  end
+
+def run  
+    puts "Calling #{@name}"
+    @callback.call if @callback
+  end
+end
+
+who_there = Event.new("Who")
+knock = Event.new("knock knock") do
+  queue.push(who_there)
+end
+
+queue.push(knock)
+queue.push(knock)
+
+loop do
+  queue.pop&.run
+end
