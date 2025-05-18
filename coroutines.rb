526652055005fdf6fa94dd17820a5999a0875adc
elasticspoon <mail@yuribocharov.dev>
2025-05-18 01:40:20 -0400

    help

---
+++ coroutines.rb
@@ -0,0 +1,39 @@
+class EventLoop
+  def initialize
+    @tasks = Queue.new
+  end
+
+  def add_coroutine(task)
+    @tasks << task
+  end
+
+  def run_coroutine(task)
+    task.resume
+    add_coroutine(task)
+  rescue FiberError
+    puts 'Task complete'
+  end
+
+  def run
+    until @tasks.empty?
+      puts 'Event loop cycle'
+      run_coroutine(@tasks.shift)
+    end
+  end
+end
+
+def fib(number)
+  Fiber.new do
+    a = 0
+    b = 1
+    number.times do |i|
+      a, b = [b, a + b]
+      puts "Fibonacci #{i}: #{a}"
+      Fiber.yield
+    end
+  end
+end
+
+loop = EventLoop.new
+loop.add_coroutine(fib(5))
+loop.run
---
+++ future_burger.rb
@@ -0,0 +1,77 @@
+class Future
+  attr_accessor :done, :fiber
+  attr_reader :result
+
+  def initialize
+    @done = false
+    @fiber = nil
+    @result = nil
+  end
+
+  def result=(result)
+    @result = result
+    @done = true
+  end
+
+  def resume
+    raise StopIteration unless done
+
+    result
+  end
+end
+
+class EventLoop
+  def initialize
+    @tasks = Queue.new
+  end
+
+  def add_coroutine(task)
+    @tasks << task
+  end
+
+  def run_coroutine(task)
+    binding.irb
+    future = task.call
+    future.fiber = task
+
+    future.resume
+    unless future.done
+      future.fiber = task
+      add_coroutine(task)
+    end
+  rescue StopIteration
+    nil
+  end
+
+  def run
+    run_coroutine(@tasks.shift) until @tasks.empty?
+  end
+end
+
+def cook(on_done)
+  burger = "Burger #{rand(20)}"
+  puts "#{burger} is cooked"
+  on_done.call(burger)
+end
+
+def cashier(burger, on_done)
+  puts "#{burger} is ready for pick up!"
+  on_done.call(burger)
+end
+
+def order_burger
+  order = Future.new
+
+  on_cashier_done = lambda { |burger|
+    puts "#{burger}? That's me! Mmmmm."
+    order.result = burger
+  }
+  on_cook_done = ->(burger) { cashier(burger, on_cashier_done) }
+
+  cook(on_cook_done)
+  order
+end
+
+loop = EventLoop.new
+loop.add_coroutine(order_burger)
+loop.run
