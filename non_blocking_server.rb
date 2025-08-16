f7f17b4b23048d7bd2e1385ef4287c909560cbe3
elasticspoon <mail@yuribocharov.dev>
2025-08-15 20:58:27 -0400

    fix: make it http

--- non_blocking_server.rb
+++ non_blocking_server.rb
@@ -1,43 +1,38 @@
 require 'socket'
 
-ADDRESS = ['127.0.0.1', 3000].freeze
+SERVER_PORT = 3000
 BUFFER_SIZE = 1024
 
 class Server
   def initialize
     puts 'Starting server...'
-    @server = Socket.new(Socket::PF_INET, Socket::SOCK_STREAM)
-    @server.bind(Addrinfo.tcp(*ADDRESS))
-    @server.listen(5)
+    @server = TCPServer.new(SERVER_PORT)
     @connections = Set.new
-    puts "server is a #{@server.class} #{@server.object_id}"
   end
 
   def accept
-    conn, addr = @server.accept_nonblock
+    conn, = @server.accept_nonblock
     puts "conn is a #{conn.class}, #{conn.object_id}"
     puts 'Connected to client...'
     @connections << conn
   rescue IO::WaitReadable
-    sleep 1
     puts 'No connection to accept. Retrying...'
   end
 
   def serve(conn)
-    loop do
-      data, = conn.recvfrom_nonblock(BUFFER_SIZE)
-      break if data.nil?
+    data, = conn.recv_nonblock(BUFFER_SIZE)
+    return if data.nil?
 
-      respond(conn, data)
-      puts "Responded to #{conn.inspect}"
-    end
+    respond(conn, data)
+    conn.close
+    @connections.delete(conn)
   rescue IO::WaitReadable
-    sleep 1
     puts 'No data to read. Retrying...'
   end
 
   def start
     loop do
+      sleep 1 if ENV['SLOW']
       accept
       @connections.each { serve(it) }
     end
@@ -45,7 +40,7 @@ class Server
     @server.close
   end
 
-  def response(conn, data)
+  def respond(conn, data)
     body = "Thanks for ordering #{data.strip} pizzas!"
     http_response = <<~RESP
       HTTP/1.1 200 OK
@@ -55,6 +50,7 @@ class Server
     RESP
 
     conn.write(http_response)
+    puts "Responded to #{conn.inspect}"
   end
 end
 
