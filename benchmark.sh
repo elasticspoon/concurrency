77af29ebc3f44e2ca014905c062a5e700c03bb5a
elasticspoon <mail@yuribocharov.dev>
2025-08-23 23:12:15 -0400

    bench

--- benchmark.sh
+++ benchmark.sh
@@ -5,10 +5,10 @@
 # echo "Benchmarking 100 Concurrent Requests (1000 Total)"
 # ab -n 10000 -c 100 http://localhost:3000/
 
-echo "Benchmarking 1 Concurrent Request (1000 Total)"
-ab -n 10 -c 1 http://localhost:3000/sleep
-echo "Benchmarking 10 Concurrent Requests (1000 Total)"
-ab -n 10 -c 5 http://localhost:3000/sleep
+# echo "Benchmarking 1 Concurrent Request (1000 Total)"
+# ab -n 10 -c 1 http://localhost:3000/sleep
+# echo "Benchmarking 10 Concurrent Requests (1000 Total)"
+# ab -n 10 -c 5 http://localhost:3000/sleep
 echo "Benchmarking 100 Concurrent Requests (1000 Total)"
 ab -n 10 -c 10 http://localhost:3000/sleep
 #
